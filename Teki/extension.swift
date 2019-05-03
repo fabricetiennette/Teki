e9b98bd2655b636ba8ac80b3eff1dc13db63b5ba
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-05-03 14:59:08 +0200

    Extension file and Customise Button

---
+++ Teki/extension.swift
@@ -0,0 +1,40 @@
+//
+//  extension.swift
+//  Teki
+//
+//  Created by Fabrice Etiennette on 03/05/2019.
+//  Copyright © 2019 Fabrice Etiennette. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    @IBInspectable var shadowOpacity: Float = 1.0 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+    @IBInspectable var shadowColor: UIColor = UIColor.gray {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat = 1.0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 12, height: 12) {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+}
